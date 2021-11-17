eb2eef616fb27f522aa56c411f607d93b5fe1210
TomasGan <tomas.gan.7@gmail.com>
2021-11-17 11:14:07 -0500

    Added classes and added the CS2 paths

---
+++ src/project5/State.java
@@ -0,0 +1,53 @@
+package project5;
+
+public class State {
+
+    private String name;
+    private LinkedList<Integer> stats;
+
+    /**
+     * Constructor initializes fields from inputs for class
+     * 
+     * @param n
+     *            name of the state
+     * @param stat
+     *            list for state statistics
+     */
+    public State(String n, LinkedList<Integer> stat) {
+        name = n;
+        stats = stat;
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    public LinkedList<Integer> getStats() {
+        
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    public String getName() {
+        
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    private LinkedList<Integer> calcStats() {
+        
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    public String toString() {
+        
+    }
+
+}
