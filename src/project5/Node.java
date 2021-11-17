f8c809e0a2ec381cd16b54ca7c8adbd62f3bc623
joell539 <joelllaury@gmail.com>
2021-11-17 14:08:23 -0500

    Made the linked list
    Added linked list along with method skeletons for the node

--- src/project5/Node.java
+++ src/project5/Node.java
@@ -1,5 +1,15 @@
 package project5;
 
 public class Node<T> {
-
+    public Node(T data) {
+        
+    }
+    
+    public T getData() {
+        
+    }
+    
+    public Node<T> getNext() {
+        
+    }
 }
---
+++ src/project5/SinglyLinkedList.java
@@ -0,0 +1,66 @@
+package project5;
+
+import java.util.Iterator;
+
+public class SinglyLinkedList<T> implements Iterable<T> {
+    private Node<T> head;
+    
+    public SinglyLinkedList(Node<T> head) {
+        this.head = head;
+    }
+    
+    public Node<T> getHead() {
+        return head;
+    }
+    
+    public void setHead(Node<T> newHead) {
+        head = newHead;
+    }
+    
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        LinkedListIterator iter = new LinkedListIterator(head);
+        sb.append("[");
+        
+        
+        
+        while (iter.hasNext()) {
+            sb.append(iter.next().toString());
+            
+            if (iter.hasNext()) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+    
+    class LinkedListIterator implements Iterator<T> {
+        private Node<T> curr;
+        
+        public LinkedListIterator(Node<T> head) {
+            curr = head;
+        }
+        
+        
+        @Override
+        public boolean hasNext() {
+            return curr.getNext() != null;
+        }
+
+        @Override
+        public T next() {
+            T data = curr.getData();
+            curr = curr.getNext();
+            return data;
+        }
+        
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
