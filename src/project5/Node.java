4144729d4d3b39eb3ed3dd46a8892485ef8e570e
akshath19 <94551488+akshath19@users.noreply.github.com>
2021-11-17 20:44:24 -0500

    Node Class & Test Class
    Node class

--- src/project5/Node.java
+++ src/project5/Node.java
@@ -1,15 +1,71 @@
 package project5;
 
 public class Node<T> {
+    private T data;
+    private Node<T> a;
+
+    /**
+     * 
+     * @param data
+     *            stores the data
+     */
     public Node(T data) {
-        
+        this.data = data;
     }
-    
+
+
+    /**
+     * takes the new entry and
+     * sets it as next node
+     * 
+     * @param entry
+     *            is the new entry being added
+     * @param node
+     *            is the new node
+     */
+
+    public Node(T entry, Node<T> node) {
+        this(entry);
+        this.setNextNode(node);
+    }
+
+
+    /**
+     * 
+     * @return returns the data
+     */
     public T getData() {
-        
+        return this.data;
     }
-    
-    public Node<T> getNext() {
-        
+
+
+    /**
+     * 
+     * @return the next node
+     */
+    public Node<T> getNextNode() {
+        return this.a;
     }
+
+
+    /**
+     * 
+     * @param nextNode
+     *            takes the latest node
+     *            and makes it the next node of the stack
+     */
+    public void setNextNode(Node<T> nextNode) {
+        this.a = nextNode;
+    }
+
+
+    /**
+     * set data
+     * 
+     * @param data
+     */
+    public void setData(T Data) {
+        this.data = Data;
+    }
+
 }
