db7fba37eeebaac061c08a5d3b2abb5149966f9a
akshath19 <94551488+akshath19@users.noreply.github.com>
2021-11-19 06:14:05 -0500

    Test methods for node
    testing for node

--- src/project5/NodeTest.java
+++ src/project5/NodeTest.java
@@ -7,10 +7,38 @@ package project5;
  *
  */
 public class NodeTest extends student.TestCase {
-    private Node<string> Node;
+    private Node<String> nodeaks;
+    private Node<String> nodebobby;
+    private Node<String> nodekkr;
+    private Node<String> nodegunhaar;
 
+    /**
+     * Set up method
+     */
     public void setUp() {
-        Node = new Node<String>("aks");
+        nodeaks = new Node<String>("aks");
+        nodebobby = new Node<String>("bobby");
+        nodekkr = new Node<String>("kkr");
 
     }
+
+
+    /**
+     * Tests the set next node node method
+     */
+    public void testsetNextNode() {
+        nodekkr.setNextNode(nodeaks);
+        assertEquals(nodeaks, "aks");
+    }
+
+
+    /**
+     * Tests the set data method
+     */
+
+    public void testsetData() {
+        nodeaks.setData("ball");
+        assertEquals(nodeaks, "ball");
+    }
+
 }
