cd9aa5c42765fe321ec4dafb311a4c67a7a956f5
akshath19 <94551488+akshath19@users.noreply.github.com>
2021-12-02 19:59:42 -0500

    covid window
    covid window

--- src/prj5/GUICovidWindow.java
+++ src/prj5/GUICovidWindow.java
@@ -7,19 +7,18 @@ import cs2.TextShape;
 import cs2.Window;
 import cs2.WindowSide;
 
-
 public class GUICovidWindow {
     private Window window;
-    
-    /**
-     * 
-     * while( i< races.getLength()) {
-     * y += BAR_SPACE_CONSTANT;
-     * System.out.println(races.getEntry(i));
-     * double height = BAR_HEIGHT_CONSTANT * races.getEntry(i).calcCFR();
-     * bar = new Shape(y, 40, BAR_WIDTH_CONSTANT, (int) height, Color.BLUE);
-     * window.addShape(bar);
-     * String CFR = races.getEntry(i).calcCFR() + "R";
-     * text = new TextShape(30, y, CFR, Color.black};
-     * window.addShape(text);
-     * i++;
+
+    public void drawRectangles() {
+        while( i< races.getLength()) {
+            y += BAR_SPACE_CONSTANT;
+            System.out.println(races.getEntry(i));
+            double height = BAR_HEIGHT_CONSTANT * races.getEntry(i).calcCFR();
+            bar = new Shape(y, 40, BAR_WIDTH_CONSTANT, (int) height, Color.BLUE);
+            window.addShape(bar);
+            String CFR = races.getEntry(i).calcCFR() + "R";
+            text = new TextShape(30, y, CFR, Color.black};
+            window.addShape(text);
+            i++;
+    }
