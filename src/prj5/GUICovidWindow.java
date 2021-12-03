8fc94428d8aa9622dd21965e1dc8631e7613f573
akshath19 <94551488+akshath19@users.noreply.github.com>
2021-12-02 19:56:31 -0500

    while loop for bars
    while loop for bars

--- src/prj5/GUICovidWindow.java
+++ src/prj5/GUICovidWindow.java
@@ -2,65 +2,24 @@ package prj5;
 
 import java.awt.Color;
 import cs2.CircleShape;
-import spacecolonies.Person;
+import cs2.Shape;
+import cs2.TextShape;
+import cs2.Window;
+import cs2.WindowSide;
+
 
 public class GUICovidWindow {
-    /**
-     * private void drawRectangles() {
-     * /**
-     * Initialize the window by adding the rectangles
-     */
-    int shapeX = QUEUE_STARTX;
-    int shapeY = QUEUE_STARTY;
-
-    /**
-         * create an object array of queue and create circle shapes for each
-         * element in
-         * the array and add to the partyCircles list
-         */
-        Object[] queuedParties = {};if(colonyCalculator.getQueue()!=null)
-    {
-        queuedParties = colonyCalculator.getQueue().toArray();
-    }for(
-    int i = 0;i<queuedParties.length;i++)
-    {
-        int shapeSize = CIRCLE_SIZE * DISPLAY_FACTOR;
-        int planetPreference = colonyCalculator.getPlanetIndex(
-            ((Person)queuedParties[i]).getPlanetPreference());
-
-        CircleShape shape = new CircleShape(shapeX, shapeY - shapeSize / 2,
-            shapeSize);
-
-        switch (planetPreference) {
-            case 0:
-                shape.setForegroundColor(PLANET_COLORS[0]);
-                shape.setBackgroundColor(Color.BLACK);
-                break;
-            case 1:
-                shape.setForegroundColor(PLANET_COLORS[1]);
-                shape.setBackgroundColor(Color.BLACK);
-                break;
-            case 2:
-                shape.setForegroundColor(PLANET_COLORS[2]);
-                shape.setBackgroundColor(Color.BLACK);
-                break;
-            default:
-                shape.setForegroundColor(PLANET_COLORS[6]);
-                shape.setBackgroundColor(Color.BLACK);
-                break;
-        }
-
-        personCircles.add(shape);
-        shapeX = shapeX + shapeSize + 5;
-    }
-
-    /**
-     * add the circles to the window
-     */
-    for(
-    int i = 0;i<personCircles.getLength();i++)
-    {
-            window.addShape(personCircles.getEntry(i));
-        }
+    private Window window;
     
-}*/}
+    /**
+     * 
+     * while( i< races.getLength()) {
+     * y += BAR_SPACE_CONSTANT;
+     * System.out.println(races.getEntry(i));
+     * double height = BAR_HEIGHT_CONSTANT * races.getEntry(i).calcCFR();
+     * bar = new Shape(y, 40, BAR_WIDTH_CONSTANT, (int) height, Color.BLUE);
+     * window.addShape(bar);
+     * String CFR = races.getEntry(i).calcCFR() + "R";
+     * text = new TextShape(30, y, CFR, Color.black};
+     * window.addShape(text);
+     * i++;
